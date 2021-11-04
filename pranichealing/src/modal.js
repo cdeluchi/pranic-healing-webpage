6c0c22a43466e0a7e0908ac2893989422054d57b
Clarice Deluchi <cdeluchi@gmail.com>
2021-11-04 17:33:53 +0100

    maps

--- pranichealing/src/modal.js
+++ pranichealing/src/modal.js
@@ -1,12 +1,75 @@
+import { useState } from "react";
+
 export default function Modal() {
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    };
+    if (modal) {
+        document.body.classList.add("active-modal");
+    } else {
+        document.body.classList.remove("active-modal");
+    }
     return (
-        <>
-            <div className="modal-container">
-                <button>Event1</button>
-                <button>Event2</button>
-                <button>Event3</button>
-                <button>Event4</button>
-            </div>
-        </>
+        <div className="modal-container">
+            <button className="btn-modal" onClick={toggleModal}>
+                Next session 1
+            </button>
+
+            {modal && (
+                <div className="modal">
+                    <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                        <h2>Next Sunday Session</h2>
+                        <p>
+                            This is the information for the next sesion on
+                            Sunday
+                        </p>
+                        <button className="close-modal" onClick={toggleModal}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+            <button className="btn-modal" onClick={toggleModal}>
+                Next session 2
+            </button>
+
+            {modal && (
+                <div className="modal">
+                    <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                        <h2>Next Sunday Session</h2>
+                        <p>
+                            This is the information for the next sesion on
+                            Sunday
+                        </p>
+                        <button className="close-modal" onClick={toggleModal}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+            <button className="btn-modal" onClick={toggleModal}>
+                Next session 3
+            </button>
+
+            {modal && (
+                <div className="modal">
+                    <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                        <h2>Next Sunday Session</h2>
+                        <p>
+                            This is the information for the next sesion on
+                            Sunday
+                        </p>
+                        <button className="close-modal" onClick={toggleModal}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
--- pranichealing/src/testimonial.js
+++ pranichealing/src/testimonial.js
@@ -1,6 +1,55 @@
 function Testimonial() {
-    return <div>
-        
-    </div>;
+    return (
+        <div className="card-wrap">
+            <div className="card-container">
+                <div className="image-container">
+                    <img src="/imghand.jpg" alt="hand" />
+                </div>
+                <div className="card-content">
+                    <div className="card-tittle">
+                        <h3>First card</h3>
+                    </div>
+                    <div className="card-body">
+                        <p>
+                            This is all the information abut this card and we
+                            will put the reviews from our patients
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="card-container">
+                <div className="image-container">
+                    <img src="/imghand.jpg" alt="hand" />
+                </div>
+                <div className="card-content">
+                    <div className="card-tittle">
+                        <h3>First card</h3>
+                    </div>
+                    <div className="card-body">
+                        <p>
+                            This is all the information abut this card and we
+                            will put the reviews from our patients
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="card-container">
+                <div className="image-container">
+                    <img src="/imghand.jpg" alt="hand" />
+                </div>
+                <div className="card-content">
+                    <div className="card-tittle">
+                        <h3>First card</h3>
+                    </div>
+                    <div className="card-body">
+                        <p>
+                            This is all the information abut this card and we
+                            will put the reviews from our patients
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Testimonial;
